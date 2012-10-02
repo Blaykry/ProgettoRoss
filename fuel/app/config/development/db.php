a042a5584d127ef157c04f4a397d6fc273a59e52
Enrico Rossignoli <erossignoli@freetechgroup.com>
2012-10-02 10:45:53 +0200

    Aggiornamento nome db

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=rapportini_db',
+			'dsn'        => 'mysql:host=localhost;dbname=progetto_db',
 			'username'   => 'root',
 			'password'   => 'ftg',
 		),
